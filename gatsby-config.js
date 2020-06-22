c08d47ac9cbf007a9181e8a8d15c0ca1f981282b
Morgan O'Neal <morgan@ghostbyte.com>
2020-06-22 14:15:04 -0700

    add more files

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,29 @@ require("dotenv").config({
 
 const target = "http://localhost:5001"
 const { FIREBASE_PROJECT_ID, FIREBASE_APP_ZONE } = process.env
+const proxyFunctions = [
+  ['frontendFunctionsPath', 'backendFunctionsPath']
+]
+let developMiddleware = () => {};
 
+if (FIREBASE_PROJECT_ID && proxyFunctions) {
+  developMiddleware = app => {
+    proxyFunctions.forEach(([frontendPath, backendPath]) => {
+      app.use(
+        `/${frontendPath}`,
+        proxy({
+          target,
+          pathRewrite: {
+            "/auth": `/${FIREBASE_PROJECT_ID}/${FIREBASE_APP_ZONE ||
+              "us-central1"}/${backendPath || frontendPath}`,
+          },
+        })
+      )
+    })
+  }
+} else {
+  console.warn('FIREBASE_PROJECT_ID env variable not found, skipping setup');
+}
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -21,19 +43,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-scss-typescript',
     `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
   ],
-  developMiddleware: app => {
-
-    app.use(
-      "/frontendFunctionsPath",
-      proxy({
-        target,
-        pathRewrite: {
-          "/auth": `/${FIREBASE_PROJECT_ID}/${FIREBASE_APP_ZONE ||
-            "us-central1"}/backendFunctionsPath`,
-        },
-      })
-    )
-
-  },
+  developMiddleware,
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+
+const Footer = ({siteTitle}) => {
+  return (
+    <footer
+      style={{
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
+      }}
+    >
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        © {new Date().getFullYear()} {` `}
+        <Link to="/">{siteTitle}</Link>. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+Footer.propTypes = {
+  siteTitle: PropTypes.string,
+};
+
+Footer.defaultProps = {
+  siteTitle: ``,
+};
+export default Footer;
